1f1200030cf23b44dd5f8ff3563aec823230e0cf
omarelhachimi <oelhachimi@solocal.com>
2023-07-19 13:43:48 +0200

    third commit

---
+++ src/main/java/com/example/fieldwire/mapper/implementation/FloorplanMapperImp.java
@@ -0,0 +1,2 @@
+package com.example.fieldwire.mapper.implementation;public class FloorplanMapperImp {
+}
---
+++ src/main/java/com/example/fieldwire/service/S3Service.java
@@ -0,0 +1,8 @@
+package com.example.fieldwire.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StorageService {
+
+}
